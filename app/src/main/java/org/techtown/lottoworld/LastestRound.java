5430253d1cdca1028eb23062e76d9f7eb5b47fbc
chaeyeon <chaney2225@gmail.com>
2022-09-20 17:13:12 +0900

    수정본

---
+++ app/src/main/java/org/techtown/lottoworld/LastestRound.java
@@ -0,0 +1,40 @@
+package org.techtown.lottoworld;
+
+import android.content.Context;
+import android.hardware.lights.LightState;
+import android.util.Log;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class LastestRound {
+    public static int round = 1029;
+    int weeks = 0;
+
+    public LastestRound() throws ParseException {
+        weeks = calculateWeeks();
+    }
+
+    public int calculateWeeks() throws ParseException {
+        Calendar getToday = Calendar.getInstance();
+        getToday.setTime(new Date()); //금일 날짜
+
+        String s_date = "2022-08-13";
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(s_date);
+        Calendar cmpDate = Calendar.getInstance();
+        cmpDate.setTime(date); //특정 일자
+
+        long diffSec = (getToday.getTimeInMillis() - cmpDate.getTimeInMillis()) / 1000;
+        long diffDays = diffSec / (24*60*60); //일자수 차이
+
+        round = 1028 + weeks;
+        weeks = (int) diffDays / 7;
+
+        return weeks;
+    }
+
+}
