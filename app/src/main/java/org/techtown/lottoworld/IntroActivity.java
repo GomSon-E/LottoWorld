025bc6c0fd28afeda8597b5767d8930655d3192f
chaeyeon <chaney2225@gmail.com>
2022-09-22 17:58:18 +0900

    다시수정

--- app/src/main/java/org/techtown/lottoworld/IntroActivity.java
+++ app/src/main/java/org/techtown/lottoworld/IntroActivity.java
@@ -92,8 +92,8 @@ public class IntroActivity extends AppCompatActivity {
         }
         int lastestRound = round.getRound();
 
-        for(int i = 1029; i <= lastestRound; i++){
-            getLottoApi(Integer.toString(i));
+        for(int i = numberQueryList.get(0).getRound(); i <= lastestRound; i++){
+            getLottoApi(i);
         }
 
     }
@@ -112,14 +112,14 @@ public class IntroActivity extends AppCompatActivity {
             Log.d("insertData", "실패함");
         }
     }
-    public void getLottoApi(String round){
+    public void getLottoApi(int round){
         Log.d("Taglog","we get into the getLottoApi function");
         if(requestQueue == null){
             requestQueue = Volley.newRequestQueue(getApplicationContext());
         }
         String[] winningNumber_ParsingIndex = {"drwtNo1","drwtNo2","drwtNo3","drwtNo4","drwtNo5","drwtNo6","bnusNo"};
 
-        String url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + round;
+        String url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + Integer.toString(round);
         StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -128,9 +128,14 @@ public class IntroActivity extends AppCompatActivity {
                     nums[i] = Integer.parseInt(""+jsonObject.get(winningNumber_ParsingIndex[i]));
                 }
                 String date = "" + jsonObject.get("drwNoDate");
-                int roundInt = Integer.parseInt(round);
                 Log.d(round + "회차 새 번호",nums.toString());
-                NumberQuery numberQuery = new NumberQuery(roundInt, date, nums);
+                NumberQuery numberQuery = new NumberQuery(round, date, nums);
+
+                try{insertNewLotto(numberQuery);}
+                catch (Exception e){
+                    e.printStackTrace();
+                    Log.d("새로운 회사 insert 실패", "몰랑랑");
+               }
             }
         }, new Response.ErrorListener() {
             // 에러처리
